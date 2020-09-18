fd8dec7245c41c0c435268252d0b52d49ed9ca81
Drew Rygh <drew.rygh@gmail.com>
2020-09-17 18:29:33 -0700

    SAN-5607 update to SDK 2.18.0 on Android and iOS
    Hopefully this fixes the crashing...

--- src/ios/OpenTokPlugin.m
+++ src/ios/OpenTokPlugin.m
@@ -57,7 +57,7 @@
     [payload setObject:@"3.4.3" forKey:@"cp_version"];
     NSMutableDictionary *logData = [[NSMutableDictionary alloc]init];
     [logData setObject:apiKey forKey:@"partner_id"];
-    [logData setObject:@"2.17.1" forKey:@"build"];
+    [logData setObject:@"2.18.0" forKey:@"build"];
     [logData setObject:@"https://github.com/opentok/cordova-plugin-opentok" forKey:@"source"];
     [logData setObject:@"info" forKey:@"payload_type"];
     [logData setObject:payload forKey:@"payload"];
